02f7aae4d1d4128dc3137273bde2702f49f8502e
Nick Ritson <nritsonsa@gmail.com>
2019-03-29 15:13:41 +0000

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreference: { backgroundThrottling: false }
+    });
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on('videos:added', (event, videos) => {
+    const promise = new Promise((resolve, reject) => {
+        ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
+            resolve(metadata);
+        });
+    });
+
+    promise.then((metadata) => {
+        console.log(metadata);
+    });
+});
